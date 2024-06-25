4d5e6d067e7d5ef5c973cef546eaaf91adba17c1
turatsinzejunior83 <45234656+turatsinzejunior83@users.noreply.github.com>
2024-06-25 09:43:46 +0200

    Update and rename rf_preds.npy to rf_preds.py

--- predictions/rf_preds.py
+++ predictions/rf_preds.py
@@ -12,7 +12,7 @@ create_directory(predictions_directory)
 
 # Define the file path for the model and the predictions
 model_file_path = 'models/random_forest.pkl'
-predictions_file_path = os.path.join(predictions_directory, 'rf_preds.npy')
+predictions_file_path = os.path.join(predictions_directory, 'rf_preds.py')
 
 # Generate some random data for making predictions
 np.random.seed(42)
@@ -24,6 +24,6 @@ rf_model = joblib.load(model_file_path)
 # Make predictions using the model
 predictions = rf_model.predict(X_test)
 
-# Save the predictions to a .npy file
-np.save(predictions_file_path, predictions)
+# Save the predictions to a .py file
+np.savetxt(predictions_file_path, predictions)
 print(f"Generated and saved {predictions_file_path}")
